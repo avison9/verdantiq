a7ee9408343d514f5140d11f9e48d8b1cf635b89
David <avisonlivingstone@gmail.com>
2025-05-22 19:39:23 +0000

    updating workflow

--- util/spark-lib-downloader.sh
+++ util/spark-lib-downloader.sh
@@ -2,13 +2,13 @@
 
 mkdir -p backend/libs/jdbc
 
-wget https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/3.3.6/hadoop-aws-3.3.6.jar -O backend/libs/jdbc/hadoop.jar
+wget https://repo1.maven.org/maven2/org/apache/hadoop/hadoop-aws/3.3.6/hadoop-aws-3.3.6.jar -O backend/libs/jdbc/hadoop-aws-3.3.6.jar
 
-wget https://repo1.maven.org/maven2/software/amazon/awssdk/s3/2.20.18/s3-2.20.18.jar -O backend/libs/jdbc/aws-sdk2.jar
+wget https://repo1.maven.org/maven2/software/amazon/awssdk/s3/2.20.18/s3-2.20.18.jar -O backend/libs/jdbc/s3-2.20.18.jar
 
-wget https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/1.12.100/aws-java-sdk-bundle-1.12.100.jar -O backend/libs/jdbc/aws-sdk2.jar
+wget https://repo1.maven.org/maven2/com/amazonaws/aws-java-sdk-bundle/1.12.100/aws-java-sdk-bundle-1.12.100.jar -O backend/libs/jdbc/aws-java-sdk-bundle-1.12.100.jar
 
-wget https://jdbc.postgresql.org/download/postgresql-42.7.3.jar -O backend/libs/jdbc/postgresql-jdbc.jar
+wget https://jdbc.postgresql.org/download/postgresql-42.7.3.jar -O backend/libs/jdbc/postgresql-42.7.3.jar
 
 wget https://repo1.maven.org/maven2/org/apache/iceberg/iceberg-spark-runtime-3.5_2.12/1.4.2/iceberg-spark-runtime-3.5_2.12-1.4.2.jar -O backend/libs/jdbc/iceberg-spark-runtime-3.5_2.12-1.4.2.jar
 
